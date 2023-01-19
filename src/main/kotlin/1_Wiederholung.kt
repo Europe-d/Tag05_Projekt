e1dba8d2a8dfe25b5aca7efde78de60d4bba8af1
europe <mavridou.evi@outlook.com>
2023-01-19 16:59:58 +0100

    Initial commit

---
+++ src/main/kotlin/1_Wiederholung.kt
@@ -0,0 +1,55 @@
+fun main() {
+
+    // TODO a: Befülle die Values
+    var danke: String = "vielen Dank"
+    println("welche ist dein Vorname:")
+    var Vorname : String = readln()
+    val NAME: String = "Helena"
+    val FIRST_NAME: String = "Impraim"
+    val LAST_NAME: String = "Mavridou"
+
+    println("Bitte schreiben sie Ihre Familiename:")
+    var Name : String = readln()
+
+    var eingabe3 = println("viele dank für die antworten")
+
+    println("welche ist dein Essen:")
+    var essen1 : String = readln()
+    var eingabe1 = println("viele dank wir kochen das für dich")
+    // TODO b: Benenne die Values
+    val TELEFONUMMER : String = "0123 45678910"
+    val FAVORITE_COLOR: String = "Blau"
+    val ESSEN : String = "KALAMARIA"
+
+    println("Bitte schreiben sie Ihre Tefenummer:")
+    var TELEFON2 : String = readln()
+    var eingabe4 = println("viele dank ich rüfe gleich an")
+    // TODO c: Befülle die Variablen neu
+    var country: String = "Spanien"
+    country = "Griecheland"
+
+    println("wo wohnen Sie;:")
+    var ort : String = readln()
+
+    var city: String = "Berlin"
+    city = "Athens"
+    
+    // TODO d: Befülle die Variable mit einer Eingabe des Users
+
+
+    // TODO e: Schlüsselwort var oder val einfügen
+    var TIME: String = "09:00"
+    var placeOfBirth: String = "München"
+    val PET_NAME: String = "Brutus"
+    var currentTVShow: String = "Breaking Bad"
+    var lastVacation: String = "Tokyo"
+    
+    
+    // TODO: Nutze Println um dem User eine kleine Textbeschreibung auszugeben
+
+
+println("MY NAME IS $NAME  $FIRST_NAME $LAST_NAME UND MEINE TELEFONUMMER IST  $TELEFONUMMER konnen sie mir anrufen wenn die bestellung nicht genommen haben")
+
+    println("wIR Sicken das essen um $TIME in Ihre straße")
+
+}
